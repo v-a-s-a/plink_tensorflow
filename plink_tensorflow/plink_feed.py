008cfc81a9e829f09e349571c4c5cefa3ea72dc8
Vassily Vladimirovich Trubetskoy <vasa@Vassilys-MacBook-Pro.local>
2018-03-07 13:35:28 +0100

    initial commit for input data feed.

---
+++ plink_tensorflow/plink_feed.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+'''
+Creating a feed for PLINK formatted data in a meta-analysis setting.
+
+Some things we would like to be able to do:
+  * Split into test/train studies.
+  * For each study, yield a batch of samples.
+  * Optionally subset to variants in a given region.
+'''
+import os
+import tensorflow as tf
+
+from pandas_plink import read_plink
+from sklearn.model_selection import train_test_split
+from random import shuffle
+import numpy as np
+
+def minibatch(X, batch_size, shuffle=True):
+    '''
+    Yield minibatches of a numpy array for training.
+    '''
+    n = X.shape[0]
+    idxs = np.arange(n)
+    if shuffle:
+        np.random.shuffle(idxs)
+
+    for i in iter(range((n // batch_size))):
+        batch_start_idx = idxs[i * batch_size]
+        data = X[batch_start_idx:(batch_start_idx + batch_size), :]
+        yield data
+
+
+class PlinkFeed:
+
+    def __init__(self, test_prop=0.8, raw_data_dir='/data/'):
+        '''
+        @test_prop: The rough proportion of sample to dedicate to training.
+        @raw_data_dir: Directory containing PLINK formatted files for each study.
+        '''
+
+        # map the input files
+         # this is a typical mount for my singu
+        root, dirs, files = next(os.walk(raw_data_dir))
+        study_plink_prefixes = [root+f.replace('.bim', '') for f in files if f.endswith('.bim')]
+        
+        # read_plink -> (bim, fam, G)
+        print('Generating Dask arrays from study PLINK files...')
+        study_arrays = {os.path.basename(f): read_plink(f) for f in study_plink_prefixes}
+        print('Done')
+
+        # We split test/train by study.
+        # We want to make sure that proportion of test/train is related to number of
+        # samples in each study.
+        # Greedy solution: Randomly select datasets for testing until the proportion
+        # of the test set is exceeded.
+        total_sample_size = float(sum([x[1].shape[0] for x in study_arrays.values()]))
+        self.test_studies = {}
+        self.train_studies = {}
+        train_set_size = 0.0
+        test_set_size = 0.0
+        studies = study_arrays.keys()
+        shuffle(studies)
+        for study in studies:
+            if train_set_size < (total_sample_size * test_prop):
+                self.train_studies[study] = study_arrays[study]
+                train_set_size += study_arrays[study][1].shape[0]
+            else:
+                self.test_studies[study] = study_arrays[study]
+                test_set_size += study_arrays[study][1].shape[0]
+
+        print('{} studies and {:.3f} of samples in training set.'.format(len(self.train_studies.keys()), train_set_size/total_sample_size))
+        print('{} studies and {:.3f} of samples in test set.'.format(len(self.test_studies.keys()), test_set_size/total_sample_size))
+
+
+    def batch_generator(self, mode, batch_size=10):
+        '''
+        Yield batches of samples from the studies in the test and train datasets.
+        '''
+        if mode == 'train':
+            for study, (bim, fam, G) in self.train_studies.items():
+                for batch in minibatch(G, batch_size=batch_size):
+                    yield batch
+        elif mode == 'test':
+            for study, (bim, fam, G) in self.test_studies.items():
+                for batch in minibatch(G, batch_size=batch_size):
+                    yield batch
+        else:
+            print('Argmument \'{}\' is not a valid mode for generating batches.'.format(mode)) 
