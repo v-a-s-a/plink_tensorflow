a6028a54113b5c16a4bed1ea4db1359173e7fb87
Vassily Vladimirovich Trubetskoy <vasa@Vassilys-MacBook-Pro.local>
2018-03-16 12:56:55 +0100

    A working model using the new data API.

--- plink_tensorflow/single_dataset.py
+++ plink_tensorflow/single_dataset.py
@@ -7,6 +7,8 @@ import dask.array as da
 import numpy as np
 import os
 
+from sklearn.model_selection import train_test_split
+
 
 class SingleDataset:
     '''
@@ -27,26 +29,38 @@ class SingleDataset:
         G = G_df.fillna(axis=0, method='backfill').values.compute().astype(np.int8) # (. _ . )
 
         # write tf.records
-        filename = os.path.join(scratch_dir, os.path.basename(plink_file) + '.tfrecords')
-        print('Writing {}...'.format(filename))
-        with tf.python_io.TFRecordWriter(filename, options=self.options) as tfwriter:
-            # write each individual gene vector to record
-            genotypes = {'genotypes_raw': tf.train.Feature(
-                            int64_list=tf.train.Int64List(value=G.flatten())),
-                         'sample_size': tf.train.Feature(int64=G.shape[1]),
-                         'num_variants': tf.train.Feature(int64=G.shape[1])}
-            example = tf.train.Example(
-                features=tf.train.Features(feature=genotypes))
-            tfwriter.write(example.SerializeToString())
-        print('Done')
+        tf_records_filenames = []
+        for sample_j in range(G.shape[1]):
+            filename = os.path.join(scratch_dir,
+                os.path.basename(plink_file) + '__' + '*'.join(self.fam.loc[self.fam.index[sample_j], ['fid', 'iid']]) + '.tfrecords')
+            # print('Writing {}...'.format(filename))
+            # with tf.python_io.TFRecordWriter(filename, options=self.options) as tfwriter:
+            #     # write each individual gene vector to record
+            #     genotypes = {'genotypes_raw': tf.train.Feature(
+            #                     bytes_list=tf.train.BytesList(value=[G[:, sample_j].tobytes()]))}
+            #     example = tf.train.Example(
+            #         features=tf.train.Features(feature=genotypes))
+            #     tfwriter.write(example.SerializeToString())
+            tf_records_filenames.append(filename)
+        # print('Done')
+
+        # split into training and test batches
+        self.train_files, self.test_files = train_test_split(tf_records_filenames,
+            test_size=0.20, random_state=42)
 
 
     def decode_tf_records(self, tfrecords_filename):
         '''
+        Parse a tf.string pointing to *.tfrecords into a genotype tensor (# variants, # samples)
+
         Helpful blog post:
         http://warmspringwinds.github.io/tensorflow/tf-slim/2016/12/21/tfrecords-guide/
         '''
-        parsed_features = tf.parse_example(tfrecords_filename,
-            {'gene_matrix': tf.FixedLenFeature((), tf.int64)})
-        return parsed_features['gene_vector']
-    
+        data = tf.parse_example([tfrecords_filename],
+            {'genotypes_raw': tf.FixedLenFeature([], tf.string)})
+
+        gene_vector = tf.decode_raw(data['genotypes_raw'], tf.int8)
+        gene_vector = tf.reshape(gene_vector, tf.stack([1, self.bim.shape[0]]))
+
+        return gene_vector
+
--- plink_tensorflow/variational_autoencoder.py
+++ plink_tensorflow/variational_autoencoder.py
@@ -11,68 +11,63 @@ import tensorflow as tf
 import tensorflow.contrib.distributions as tfd
 from tqdm import tqdm
 from tensorflow.python import debug as tf_debug
+from sklearn.model_selection import train_test_split
 
 from single_dataset import SingleDataset
 
 
 class BasicVariationalAutoencoder():
 
-    def __init__(self, batch_size = 100, latent_dim = 2, epochs = 50):
-        
+    def __init__(self, batch_size = 100, latent_dim = 2, epochs = 50,):
+
         # Data input
         self.input_dataset = SingleDataset(plink_file='/plink_tensorflow/data/hapmap1',
             scratch_dir='/plink_tensorflow/data/')
+        self.m_variants = self.input_dataset.bim.shape[0]
 
         print('Building computational graph.')
-        self.filenames = tf.placeholder(tf.string, shape=[], name='tf_records_filename')
+        self.filenames = tf.placeholder(tf.string, shape=[None], name='tf_records_filename')
         self.dataset = tf.data.TFRecordDataset(self.filenames, compression_type=tf.constant('ZLIB'))
         self.dataset = self.dataset.map(self.input_dataset.decode_tf_records)
-        
+        # self.dataset = self.dataset.repeat(epochs)
+        # self.dataset = self.dataset.batch(10)
+
         self.iterator = self.dataset.make_initializable_iterator()
+        genotypes = self.iterator.get_next()
+
+        genotypes = tf.cast(genotypes, tf.float32)
+        genotypes.set_shape([None, self.m_variants])
+
         print('Done')
 
-        # self.dataset = tf.data.TFRecordDataset.concatenate(self.dataset)
-        # shape = self.dataset.shape
-        # TODO: merge the tensors from individual studies
-        # TODO: calculate correct batch size
-        # self.dataset = self.dataset.batch(10)
-        # iterator = self.dataset.make_initializable_iterator()
-        # next_element = iterator.get_next()
-
         # Define the model.
-        # prior = self._make_prior(latent_dim=2)
-        # make_encoder = tf.make_template('encoder', self._make_encoder) 
-        # posterior = make_encoder(self.dataset, latent_dim=2)
-        # self.latent_z = posterior.sample()
+        prior = self._make_prior(latent_dim=2)
+        make_encoder = tf.make_template('encoder', self._make_encoder) 
+        posterior = make_encoder(genotypes, latent_dim=2)
+        self.latent_z = posterior.sample()
 
-        # # Define the loss.
-        # make_decoder = tf.make_template('decoder', self._make_decoder)
-        # likelihood = make_decoder(self.latent_z,
-        #     [self.input_dataset.m_variants]).log_prob(next_element)
-        # divergence = tfd.kl_divergence(posterior, prior)
-        # self.elbo = tf.reduce_mean(likelihood - divergence)
-        # self.optimizer = tf.train.AdamOptimizer(0.001).minimize(-self.elbo)
+        # Define the loss.
+        make_decoder = tf.make_template('decoder', self._make_decoder)
+        likelihood = make_decoder(self.latent_z).log_prob(genotypes)
+        divergence = tfd.kl_divergence(posterior, prior)
+        self.elbo = tf.reduce_mean(likelihood - divergence)
+        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(-self.elbo)
 
 
     def infer_parameters(self):
         # with tf_debug.LocalCLIDebugWrapperSession(tf.train.MonitoredSession()) as sess:
         print('Executing compute graph.')
         with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for epoch in tqdm(range(50)):
+                sess.run(self.iterator.initializer,
+                    feed_dict={self.filenames: self.input_dataset.test_files})
+                test_elbo, test_codes = sess.run([self.elbo, self.latent_z])
 
-
-            # sess.run(self.iterator.initializer,
-            #     feed_dict={self.filenames: self.input_dataset.plink_file})
-            sess.run(self.iterator.initializer, feed_dict={self.filenames: self.input_dataset.plink_file + '.tfrecords'})
-            print(sess.run(self.iterator.get_next()))
-
-            # for epoch in tqdm(range(50)):
-            #     self.input_dataset.test_train_split()
-            #     test_feed = {self.data: self.input_dataset.test_set()}
-            #     test_elbo, test_codes = sess.run([self.elbo, self.latent_z], test_feed)
-            #     print('Epoch', epoch, 'elbo', test_elbo)
-            #     for training_batch in tqdm(self.input_dataset.train_set_minibatches()):
-            #         train_feed = {self.data: training_batch}
-            #         sess.run(self.optimizer, train_feed)
+                sess.run(self.iterator.initializer,
+                    feed_dict={self.filenames: self.input_dataset.train_files})
+                print('Epoch', epoch, 'elbo', test_elbo)
+                sess.run(self.optimizer)
         
         print('Done')
 
@@ -91,12 +86,11 @@ class BasicVariationalAutoencoder():
         return tfd.MultivariateNormalDiag(loc, scale)
 
 
-    def _make_decoder(self, z, data_shape):
+    def _make_decoder(self, z):
         x = tf.layers.dense(z, 200, tf.nn.relu)
-        x = tf.layers.dense(x, 200, tf.nn.relu)
-        logit = tf.layers.dense(x)
-        logit = tf.reshape(logit, [-1] + logit.shape)
-        return tfd.Independent(tfd.Binomial(logits=logit, total_count=2.))
+        logits = tf.layers.dense(x, 200, tf.nn.relu)
+        logits = tf.layers.dense(x, self.m_variants)
+        return tfd.Independent(tfd.Binomial(logits=logits, total_count=2.))
 
 
 if __name__ == '__main__':
