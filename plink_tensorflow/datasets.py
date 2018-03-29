511f9d798bc0546136c63e1984051b00f75c3dcb
Vassily Trubetskoy <vasa@Vassilys-MacBook-Pro.local>
2018-03-29 10:46:24 +0200

    Stash before trying a feedable iterator

--- plink_tensorflow/datasets.py
+++ plink_tensorflow/datasets.py
@@ -4,6 +4,8 @@
 import tensorflow as tf
 from pandas_plink import read_plink
 import dask.array as da
+import dask.dataframe as dd
+import pandas as pd
 import numpy as np
 import os
 
@@ -15,38 +17,49 @@ class SingleDataset:
     Single PLINK formatted dataset for input to the variational autoencoder.
     '''
 
-    def __init__(self, plink_file, scratch_dir):
+    def __init__(self, plink_file, scratch_dir, overwrite=False):
         self.options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.ZLIB)
         self.plink_file = plink_file
+        self.scratch_dir = scratch_dir
 
         # read plink data
         print('Reading PLINK data...')
         self.bim, self.fam, G = read_plink(plink_file)
+        # import ipdb; ipdb.set_trace()
         print('Done')
 
-        # fill missing values
-        G_df = G.to_dask_dataframe()
-        G = G_df.fillna(axis=0, method='backfill').values.compute().astype(np.int8) # (. _ . )
-
         # write tf.records
-        tf_records_filenames = []
-        for sample_j in range(G.shape[1]):
-            filename = os.path.join(scratch_dir,
-                os.path.basename(plink_file) + '__' + '*'.join(self.fam.loc[self.fam.index[sample_j], ['fid', 'iid']]) + '.tfrecords')
-            # print('Writing {}...'.format(filename))
-            # with tf.python_io.TFRecordWriter(filename, options=self.options) as tfwriter:
-            #     # write each individual gene vector to record
-            #     genotypes = {'genotypes_raw': tf.train.Feature(
-            #                     bytes_list=tf.train.BytesList(value=[G[:, sample_j].tobytes()]))}
-            #     example = tf.train.Example(
-            #         features=tf.train.Features(feature=genotypes))
-            #     tfwriter.write(example.SerializeToString())
-            tf_records_filenames.append(filename)
-        # print('Done')
+        if overwrite:
+            G_df = dd.from_dask_array(da.transpose(G))
+            G_df = G_df.fillna(value=1) # (. _ . )
+            G_df = G_df.astype(np.int8)
+            # G_df = G_df.loc[0:200, :]
+            tf_records_filenames = G_df.apply(self._write_records, axis=1).compute()
+        else:
+            root, dirs, files = next(os.walk(scratch_dir))
+            tf_records_filenames = [root + f for f in files if f.endswith('.tfrecords')]
+
+        print('Done')
 
         # split into training and test batches
         self.train_files, self.test_files = train_test_split(tf_records_filenames,
             test_size=0.20, random_state=42)
+
+
+    def _write_records(self, row):
+
+        sample_j = row.name
+        filename = os.path.join(self.scratch_dir,
+            os.path.basename(self.plink_file) + '__' + '*'.join(self.fam.loc[self.fam.index[sample_j], ['fid', 'iid']]) + '.tfrecords')
+
+        print('Writing {}...'.format(filename))
+        with tf.python_io.TFRecordWriter(filename, options=self.options) as tfwriter:
+            genotypes = {'genotypes_raw': tf.train.Feature(
+                            bytes_list=tf.train.BytesList(value=[row.values.tostring()]))}
+            example = tf.train.Example(
+                features=tf.train.Features(feature=genotypes))
+            tfwriter.write(example.SerializeToString())
+        return filename
 
 
     def decode_tf_records(self, tfrecords_filename):
@@ -59,7 +72,7 @@ class SingleDataset:
         data = tf.parse_example([tfrecords_filename],
             {'genotypes_raw': tf.FixedLenFeature([], tf.string)})
 
-        gene_vector = tf.decode_raw(data['genotypes_raw'], tf.int8)
+        gene_vector = tf.decode_raw(data['genotypes_raw'], np.int8)
         gene_vector = tf.reshape(gene_vector, [self.bim.shape[0]])
 
         return gene_vector
--- plink_tensorflow/variational_autoencoder.py
+++ plink_tensorflow/variational_autoencoder.py
@@ -18,12 +18,14 @@ from datasets import SingleDataset
 
 class BasicVariationalAutoencoder():
 
-    def __init__(self, batch_size = 100, latent_dim = 25, epochs = 50):
+    def __init__(self, batch_size = 1000, latent_dim = 25, epochs = 50):
 
         # Data input
-        self.input_dataset = SingleDataset(plink_file='/plink_tensorflow/data/hapmap1',
-            scratch_dir='/plink_tensorflow/data/')
+        self.input_dataset = SingleDataset(plink_file='/plink_tensorflow/data/test/scz_easy-access_wave2.no_trio.bgn',
+            scratch_dir='/plink_tensorflow/data/test/',
+            overwrite=False)
         self.m_variants = self.input_dataset.bim.shape[0]
+        self.latent_dim = latent_dim
 
         print('Building computational graph.')
         self.filenames = tf.placeholder(tf.string, shape=[None], name='tf_records_filename')
@@ -35,15 +37,15 @@ class BasicVariationalAutoencoder():
         self.iterator = self.dataset.make_initializable_iterator()
         genotypes = self.iterator.get_next()
 
-        genotypes = tf.cast(genotypes, tf.float32)
+        genotypes = tf.cast(genotypes, tf.float32, name='cast_genotypes')
         genotypes.set_shape([None, self.m_variants])
 
         print('Done')
 
         # Define the model.
-        prior = self._make_prior(latent_dim=2)
+        prior = self._make_prior(latent_dim=self.latent_dim)
         make_encoder = tf.make_template('encoder', self._make_encoder) 
-        posterior = make_encoder(genotypes, latent_dim=2)
+        posterior = make_encoder(genotypes, latent_dim=self.latent_dim)
         self.latent_z = posterior.sample()
 
         # Define the loss.
@@ -55,9 +57,9 @@ class BasicVariationalAutoencoder():
 
 
     def infer_parameters(self):
-        # with tf_debug.LocalCLIDebugWrapperSession(tf.train.MonitoredSession()) as sess:
         print('Executing compute graph.')
-        with tf.Session() as sess:
+        with tf_debug.LocalCLIDebugWrapperSession(tf.Session()) as sess:
+        # with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             for epoch in tqdm(range(50)):
                 # test set
@@ -66,7 +68,8 @@ class BasicVariationalAutoencoder():
                 while True:
                     # consume batches
                     try:
-                        test_elbo, test_codes = sess.run([self.elbo, self.latent_z])
+                        test_elbo = sess.run(self.elbo,
+                            feed_dict={self.filenames: self.input_dataset.train_files})
                         print('Epoch', epoch, 'elbo', test_elbo)
                     except tf.errors.OutOfRangeError:
                         break
@@ -85,8 +88,8 @@ class BasicVariationalAutoencoder():
 
 
     def _make_encoder(self, data, latent_dim):
-        x = tf.layers.dense(data, 512, tf.nn.relu)
-        x = tf.layers.dense(x, 512, tf.nn.relu)
+        x = tf.layers.dense(data, 200, tf.nn.relu)
+        x = tf.layers.dense(x, 200, tf.nn.relu)
         loc = tf.layers.dense(x, latent_dim)
         scale = tf.layers.dense(x, latent_dim, tf.nn.softplus)
         return tfd.MultivariateNormalDiag(loc, scale)
@@ -99,10 +102,10 @@ class BasicVariationalAutoencoder():
 
 
     def _make_decoder(self, z):
-        x = tf.layers.dense(z, 512, tf.nn.relu)
-        logits = tf.layers.dense(x, 512, tf.nn.relu)
+        x = tf.layers.dense(z, 200, tf.nn.relu)
+        x = tf.layers.dense(x, 200, tf.nn.relu)
         logits = tf.layers.dense(x, self.m_variants)
-        return tfd.Independent(tfd.Binomial(logits=logits, total_count=2.))
+        return tfd.Independent(tfd.Binomial(logits=logits, total_count=2.), reinterpreted_batch_ndims=1)
 
 
 if __name__ == '__main__':
